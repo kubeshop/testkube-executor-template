1d9e900625465382c6c246fee4bc0ad4e84e8b5f
Jacek Wysocki <jacek.wysocki@kinguin.io>
2021-09-01 19:38:33 +0200

    moved to kubtest libs

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -2,33 +2,34 @@ package main
 
 import (
 	"github.com/kelseyhightower/envconfig"
-	"github.com/kubeshop/kubtest-executor-template/internal/app/executor"
-	"github.com/kubeshop/kubtest-executor-template/internal/pkg/repository/result"
-	"github.com/kubeshop/kubtest-executor-template/internal/pkg/storage"
+	"github.com/kubeshop/kubtest-executor-template/pkg/runner"
+	"github.com/kubeshop/kubtest/pkg/executor/repository/result"
+	"github.com/kubeshop/kubtest/pkg/executor/repository/storage"
+	"github.com/kubeshop/kubtest/pkg/executor/server"
+	"github.com/kubeshop/kubtest/pkg/ui"
 )
 
-const DatabaseName = "template-executor"
-
 type MongoConfig struct {
-	DSN string `envconfig:"EXECUTOR_MONGO_DSN" default:"mongodb://localhost:27017"`
-	DB  string `envconfig:"EXECUTOR_MONGO_DB" default:"template-executor"`
+	DSN        string `envconfig:"EXECUTOR_MONGO_DSN" default:"mongodb://localhost:27017"`
+	DB         string `envconfig:"EXECUTOR_MONGO_DB" default:"example-executor"`
+	Collection string `envconfig:"EXECUTOR_MONGO_COLLECTION" default:"executions"`
 }
 
-var Config MongoConfig
+var cfg MongoConfig
 
 func init() {
-	envconfig.Process("mongo", &Config)
+	envconfig.Process("mongo", &cfg)
 }
 
 func main() {
-
-	db, err := storage.GetMongoDataBase(Config.DSN, Config.DB)
+	db, err := storage.GetMongoDataBase(cfg.DSN, cfg.DB)
 	if err != nil {
 		panic(err)
 	}
 
-	exec := executor.NewTemplateExecutor(result.NewMongoRespository(db))
-	exec.Init()
-	panic(exec.Run())
+	repo := result.NewMongoRespository(db, cfg.Collection)
+	runner := runner.NewRunner()
+	exec := server.NewExecutor(repo, runner)
 
+	ui.ExitOnError("Running executor", exec.Init().Run())
 }
--- internal/pkg/server/endpoints.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import "github.com/gofiber/fiber/v2"
-
-func (s HTTPServer) HealthEndpoint() fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		return c.SendString("OK 👋!")
-	}
-}
