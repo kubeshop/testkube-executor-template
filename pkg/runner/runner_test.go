d6acb94f8eafc341ab7f226f098a9a16e5725b85
Vladislav Sukhin <vladislav@kubeshop.io>
2022-05-03 13:48:46 +0300

    fix: add git dep and update api dep (#18)

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -20,7 +20,7 @@ func TestRun(t *testing.T) {
 
 		// then
 		assert.NoError(t, err)
-		assert.Equal(t, result.Status, testkube.ExecutionStatusSuccess)
+		assert.Equal(t, result.Status, testkube.ExecutionStatusPassed)
 	})
 
 }
